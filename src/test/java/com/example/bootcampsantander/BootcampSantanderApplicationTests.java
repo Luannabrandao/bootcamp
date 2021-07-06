503c21a2db11e7267376904515635f0ab3ffd631
Luanna <lu_rotelli@outlook.com>
2021-07-06 18:08:33 -0300

    criação de repositório com o código do bootcamp

---
+++ src/test/java/com/example/bootcampsantander/BootcampSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcampsantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
